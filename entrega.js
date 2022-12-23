4ba63c7bf4a25afab5766cf9f1005b7935746b4b
adriandileo <adrian.dileo@bue.edu.ar>
2022-12-23 05:26:50 -0300

    entrega 1

--- entrega.js
+++ entrega.js
@@ -1,3 +1,57 @@
-const numero = 1
+class Producto{
+  #productos=[]
+  constructor(){
+    this.id=0
+  }
+  evento(title,description,price,code,stock=25){
+    this.id++
+    const products={
+      id:this.id,
+      title,
+      description,
+      price,
+      code,
+      stock
+    }
+    this.#productos.push(products);
+    
+   }
+   getProducts(){
+    return this.#productos
+   }
+   getProductsById(id){
+    const byId= this.#productos.filter(byId => byId.id===id )
+    if(byId.length===0){
+      return `Sin id: ${id}`
+    }else{
+    return byId }
+   }
 
-console.log(numero)
+}
+
+
+let addProducto= new Producto()
+let title="Prueba"
+let description="Producto de prueba"
+let price=200
+let code = "qq"
+
+let title1="Prueba1"
+let description1="Producto de prueba 1"
+let price1=200
+let code1 = "ee"
+
+let title2="Prueba2"
+let description2="Producto de prueba 2"
+let price2=200
+let code2 = "ww"
+
+addProducto.evento(title,description,price,code)
+addProducto.evento(title1,description1,price1,code1)
+addProducto.evento(title2,description2,price2,code2)
+
+console.log(
+addProducto.getProductsById(2),
+addProducto.getProducts()
+
+)
