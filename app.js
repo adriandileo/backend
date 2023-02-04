c6c8c8b194ad796ce3949ee2aa44ba722e511826
adriandileo <adrian.dileo@bue.edu.ar>
2023-02-04 11:24:53 -0300

    handlebars

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
 const express = require('express')
 const morgan = require('morgan')
-const handlebars = requiere('express-handlebars')
+const handlebars = require('express-handlebars')
 const productosRoutes = require('./routes/productos')
 const cartWidgetRoutes= require('./routes/cartwidget')
+const Server = require('socket.io')
+
 const port = 8000
 const app=express()
+//const httpServer=
+app.listen(port,() =>{
+    console.log(`Server running at port ${port}`)
+})
+
+//const socketServer= new Server (httpServer)
+
 
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(express.urlencoded({extended:true}))
 app.engine('handlebars',handlebars.engine())
-app.set('view',__dirname+'/view')
+app.set('view',__dirname +'/view')
 app.set('view engeniere','handlebars')
-app.set(express.static(__dirname+'/public'))
+app.use(express.static(__dirname+'/public'))
 
 
 
 app.use('/productos',productosRoutes)
 app.use('/cart',cartWidgetRoutes)
 
-app.listen(port,() =>{
-    console.log(`Server running at port ${port}`)
-})
+
+app.get('/',(req,res)=>{
+    let  user = {
+        name:"asda",
+        lastname: "tere"
+    }
+    // res.json({message:user})
+    res.render('index.handlebars',{})
+} )
